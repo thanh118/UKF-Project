b173359361c1c01130b520227b1186d0e54bf301
Thanh <thanhnguyen1181991@gmail.com>
2018-11-18 16:25:57 +0100

    Commit 4

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -217,6 +217,35 @@ void UKF::Prediction(double delta_t) {
 
   //create augmented covariance matrix
   P_aug.fill(0.0);
+  P_aug.topLeftCorner(5, 5) = P_;
+  P_aug(5, 5) = std_a_*std_a_;
+  P_aug(6, 6) = std_yawdd_*std_yawdd_;
+
+  //create square root matrix
+  MatrixXd L = P_aug.llt().matrixL();
+
+  //create augmented sigma points
+  Xsig_aug.col(0) = x_aug;
+  for(int i = 0; i < n_aug_; i++){
+    Xsig_aug.col(i+1) = x_aug + sqrt(lambda_ + n_aug_) * L.col(i);
+    Xsig_aug.col(i+1 + n_aug_) = x_aug - sqrt(lambda_ + n_aug_)* L.col(i);
+  }
+
+  /*****************************************************************************
+  *  Predict Sigma Points
+  ****************************************************************************/
+  //predict sigma points
+
+  for(int i = 0; i < 2 * n_aug_+1; i++){
+    //extract values for better readability
+    double p_x = Xsig_aug(0, i);
+    double p_y = Xsig_aug(1, i);
+    double v = Xsig_aug(2, i);
+    double yaw = Xsig_aug(3, i);
+    double yawd = Xsig_aug(4, i);
+    double nu_a = Xsig_aug(5, i);
+    double nu_yawdd = Xsig_aug(6, i);
+  }
 }
 
 /**
